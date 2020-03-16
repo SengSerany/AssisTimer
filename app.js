a204a1ed42f3af2e5ad8dfe88da8edff149f1565
SengSerany <cranyseng@gmail.com>
2020-03-16 01:39:27 +0100

    begin to make the timer function on the one page

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
 
 const timer = require('./routes/timer_routes')
 
 const app = express();
+
+require('dotenv').config();
+
+const mongoDB = process.env.mongoDBCluster;
+mongoose.connect(mongoDB, {useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true }, () =>{});
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log(`### We're connected to database ! ###`)
+});
 
 app.set('view engine', 'ejs');
 app.use('/', timer);
