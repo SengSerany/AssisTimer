426f7cb6178c00d6d0ec3f2e7cd03ff81ed0e907
SengSerany <cranyseng@gmail.com>
2020-03-27 18:14:19 +0100

    Timer is fonctionnal

--- public/js/timer.js
+++ public/js/timer.js
@@ -5,8 +5,7 @@ button.addEventListener("click", timerField);
 
 function timerField() {
 
-    timeSetter.innerHTML = ""
-    
+    timeSetter.innerHTML = "";
 
     let subtitleDiv = document.createElement("div");
     subtitleDiv.classList.add("subtitle");
@@ -20,10 +19,55 @@ function timerField() {
     timeWatchDiv.classList.add("timeWatch");
 
     let newP = document.createElement("p");
+    newP.setAttribute("id", "timer")
     let lineBreak = document.createElement("br");
-    let newTextP = document.createTextNode(`00:00`);
+
     newP.appendChild(lineBreak);
-    newP.appendChild(newTextP);
+    
+    let second = 00;
+    let minute = 00;
+
+    function setTimer(){
+        
+        let counter = window.setInterval(() => {
+            
+            if (minute == 25 && second == 1 ){
+                clearInterval(counter);
+            } else if (minute > 9 && second >= 10) {
+                document.getElementById("timer").innerHTML = `${minute}:${second}`;
+                second = second + 1;
+                if (second == 60){
+                    clearInterval(counter);
+                    minute = minute + 1;
+                    second = 0;
+                    setTimer();
+                }
+
+            } else if (minute < 10 && second >= 10){
+                document.getElementById("timer").innerHTML = `0${minute}:${second}`;
+                second = second + 1;
+                if (second == 60){
+                    clearInterval(counter);
+                    minute = minute + 1;
+                    second = 0;
+                    setTimer();
+                }
+            } else if(minute >= 10 && second < 10){
+                document.getElementById("timer").innerHTML = `${minute}:0${second}`;
+                second = second + 1;
+            } else if (minute < 10 && second < 10){
+                document.getElementById("timer").innerHTML = `0${minute}:0${second}`;
+                second = second + 1;
+            } else if (minute == 0 && second == 0){
+                document.getElementById("timer").innerHTML = "00:00";
+                second = second + 1;
+            } 
+
+
+        }, 1000);
+    };
+
+    setTimer();
 
     timeWatchDiv.appendChild(newP);
 
